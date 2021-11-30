992614d5e0e629f02ee77f6c2fba9c748c6e4448
asiapiorko <64212078+asiapiorko@users.noreply.github.com>
2021-11-30 10:58:29 +0100

    Create Problem_4.py

---
+++ Midterm_Exam/Problem_4.py
@@ -0,0 +1,24 @@
+
+# Problem 4
+# 10/10 points (graded)
+# Implement a function called closest_power that meets the specifications below.
+
+# def closest_power(base, num):
+#     '''
+#     base: base of the exponential, integer > 1
+#     num: number you want to be closest to, integer > 0
+#     Find the integer exponent such that base**exponent is closest to num.
+#     Note that the base**exponent may be either greater or smaller than num.
+#     In case of a tie, return the smaller value.
+#     Returns the exponent.
+#     '''
+#     # Your code here
+
+#For example,
+# closest_power(3,12) returns 2
+# closest_power(4,12) returns 2
+# closest_power(4,1) returns 0
+
+ 
+
+# Paste your function here

40959b8637f318cc72575e0e518ff1cdad1da751
asiapiorko <64212078+asiapiorko@users.noreply.github.com>
2021-12-03 09:31:16 +0100

    Update Problem_3_-_CiphertextMessage.py

--- Problem_Set_5/Problem_3_-_CiphertextMessage.py
+++ Problem_Set_5/Problem_3_-_CiphertextMessage.py
@@ -74,13 +74,4 @@ class CiphertextMessage(Message):
                         print('Expected Output:',result_tuple)
                         return element
 
-#Example test case (PlaintextMessage)
-# plaintext = PlaintextMessage('hello', 2)
-# print('Expected Output: jgnnq')
-# print('Actual Output:', plaintext.get_message_text_encrypted())
-    
-# #Example test case (CiphertextMessage)
-# ciphertext = CiphertextMessage('jgnnq')
-# print('Expected Output:', (24, 'hello'))
-# print('Actual Output:', ciphertext.decrypt_message())
 
